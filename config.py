c5a47b60a9f754a038d1aa56fd9465a5612bdd2f
Zhibo Liu <zhiboliu97@gmail.com>
2022-07-02 22:15:25 +0800

    collect global variable info

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
 csmith_runtime = '/home/lifter/Documents/csmith/runtime'
-clang_dwarf_cmd = "clang -c -w -g -emit-llvm -S {} -o {}"
-clang_dwarf_cmd2 = "clang -c -w -g -emit-llvm -S -I/home/lifter/Documents/csmith/runtime {} -o {}"
+clang_ir_cmd = "clang -c -w -g -O0 -emit-llvm -S {} -o {}"
+clang_ir_cmd2 = "clang -c -w -g -O0 -emit-llvm -S -I/home/lifter/Documents/csmith/runtime {} -o {}"
+clang_dwarf_cmd2 = "clang -w -g -O0 -I/home/lifter/Documents/csmith/runtime {} -o {}"
 
-dwarfdump_cmd = "llvm-dwarfdump {} > {}"
+dwarfdump_cmd = "llvm-dwarfdump-12 {} > {}"
 
 emsdk_path = '/home/lifter/Documents/WebAssembly/emsdk'
 emscripten_path = '/home/lifter/Documents/WebAssembly/emsdk/upstream/emscripten'
 node_path = '/home/lifter/Documents/WebAssembly/emsdk/node/14.18.2_64bit/bin'
 
 emcc_cmd = 'emcc -w -O3 -I' + csmith_runtime + ' {} -o {} -o {}'
+emcc_dwarf_cmd = 'emcc -w -g -O3 -I' + csmith_runtime + ' {} -o {} -o {}'
 nodejs_cmd = 'node {}'
--- profile.py
+++ profile.py
@@ -13,7 +13,7 @@ def compile_llvm_ir(c_src_path: str):
     tmp_dir = utils.project_dir
     utils.project_dir = dir_path
 
-    status, output = utils.cmd(config.clang_dwarf_cmd2.format(c_src_path, ll_path))
+    status, output = utils.cmd(config.clang_ir_cmd2.format(c_src_path, ll_path))
 
     utils.project_dir = tmp_dir
     return ll_path
@@ -60,7 +60,7 @@ def emscripten_dwarf(c_src_path: str):
     tmp_dir = utils.project_dir
     utils.project_dir = dir_path
 
-    stdout, stderr = utils.cmd_emsdk(config.emcc_cmd.format(c_src_path, wasm_path, js_path))
+    stdout, stderr = utils.cmd_emsdk(config.emcc_dwarf_cmd.format(c_src_path, wasm_path, js_path))
     status, output = utils.cmd(config.dwarfdump_cmd.format(wasm_path, dwarf_txt_path))
 
     utils.project_dir = tmp_dir
@@ -68,9 +68,94 @@ def emscripten_dwarf(c_src_path: str):
     return wasm_path, js_path, dwarf_txt_path
 
 
-def 
+def clang_dwarf(c_src_path: str):
+    c_src_path = os.path.abspath(c_src_path)
+    dir_path = os.path.dirname(c_src_path)
+    assert c_src_path.endswith('.c')
+    out_path = c_src_path[:-2] + '.out'
+    dwarf_txt_path = out_path + '.dwarf'
+
+    tmp_dir = utils.project_dir
+    utils.project_dir = dir_path
+
+    status, output = utils.cmd(config.clang_dwarf_cmd2.format(c_src_path, out_path))
+    status, output = utils.cmd(config.dwarfdump_cmd.format(out_path, dwarf_txt_path))
+
+    utils.project_dir = tmp_dir
+
+    return out_path, dwarf_txt_path
+
+
+def parse_dwarf_obj(obj_str: str):
+    mat = re.match(r"(0x\w+):", obj_str)
+    obj_addr = mat.group(1)
+    obj_str = obj_str[11:].strip()
+    lines = obj_str.split('\n')
+    obj_type = lines[0].strip()
+
+    obj_dict = dict()
+    obj_dict["addr"] = obj_addr
+    obj_dict["type"] = obj_type
+
+    obj_str = obj_str[obj_str.find('\n'):].strip()  # skip the first line
+    while mat := re.match(r"(DW_\w+)\t(\([^\[\n\r]*(\s+\[[^)\n\r]+\)[^)\n\r]*)*.*\))", obj_str):
+        attr = mat.group(1)
+        value = mat.group(2)
+        obj_dict[attr] = value
+
+        obj_str = obj_str.replace(mat.group(), '')
+        obj_str = obj_str.strip()
+    return obj_addr, obj_type, obj_dict
+
+
+def traverse_dwarf(dwarf_path: str, filter='DW_OP_addr'):
+    glob_objs = []
+    var_names_list = []
+    with open(dwarf_path, 'r') as f:
+        dwarf_txt = f.read()
+        dwarf_objs = dwarf_txt.split('\n\n')
+        for obj in dwarf_objs:
+            if not obj.startswith('0x') or "DW_TAG_variable" not in obj or "DW_AT_name" not in obj:
+                continue
+
+            obj_addr, obj_type, obj_dict = parse_dwarf_obj(obj)
+            if obj_type == "DW_TAG_variable" and \
+                    "DW_AT_name" in obj_dict.keys() and \
+                    "DW_AT_location" in obj_dict.keys() and filter in obj_dict["DW_AT_location"]:
+                glob_objs.append((obj_addr, obj_dict))
+                var_names_list.append(obj_dict["DW_AT_name"])
+    return glob_objs, var_names_list
+
+
+def collect_glob_vars(c_src_path: str):
+    out_path, clang_dwarf_txt_path = clang_dwarf(c_src_path)
+    wasm_path, js_path, wasm_dwarf_txt_path = emscripten_dwarf(c_src_path)
+
+    wasm_glob_objs, wasm_name_list = traverse_dwarf(wasm_dwarf_txt_path)
+    clang_glob_objs, clang_name_list = traverse_dwarf(clang_dwarf_txt_path)
+
+    new_wasm_globs = []
+    new_clang_globs = []
+    for obj in clang_glob_objs:
+        if obj[1]["DW_AT_name"] in wasm_name_list:
+            new_clang_globs.append(obj)
+            for wasm_obj in wasm_glob_objs:
+                if wasm_obj[1]["DW_AT_name"] == obj[1]["DW_AT_name"]:
+                    new_wasm_globs.append(wasm_obj)
+                    break
+            continue
+
+    # debug
+    print(new_wasm_globs)
+    print(new_clang_globs)
+
+    status, output = utils.cmd("rm {}".format(out_path))
+    status, output = utils.cmd("rm {}".format(wasm_path))
+    status, output = utils.cmd("rm {}".format(js_path))
 
 
 if __name__ == '__main__':
-    get_global_vars("/home/lifter/Documents/WebAssembly/examples/test1090_re.c")
+    # get_global_vars("/home/lifter/Documents/WebAssembly/examples/test1090_re.c")
+
+    collect_glob_vars("/home/lifter/Documents/WebAssembly/examples/test1090_re.c")
 
