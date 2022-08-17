2966123105c1254b6a96adc91ce7816bd194fa12
Zhibo Liu <zhiboliu97@gmail.com>
2022-08-17 23:01:34 +0800

    update

--- test_wasm_opt.py
+++ test_wasm_opt.py
@@ -24,26 +24,36 @@ def simple_test(process_idx: int):
             utils.get_one_csmith(c_path)
 
             wasm_path, js_path, wasm_dwarf_txt_path = profile.emscripten_dwarf(c_path, opt_level='-O0')
-            elf_path, dwarf_path = profile.clang_dwarf(c_path, opt_level='-O3')
+            elf_path, dwarf_path = profile.clang_dwarf(c_path, opt_level='-O0')
 
-            output1, status = utils.run_single_prog(elf_path)
-            output2, status = utils.run_single_prog("node {}".format(js_path))
+            output1, status1 = utils.run_single_prog(elf_path)
+            output2, status2 = utils.run_single_prog("node {}".format(js_path))
+            if status1 or status2:
+                break  # one of the program did not return normally
+
             if output1 == output2:
                 break  # exclude FPs caused by Undefined Behaviors
 
         wasm_path, wasm_dwarf_txt_path = utils.wasm_opt(wasm_path, wasm_opt_level='-O4')
 
         # lightweight checking
-        output1, status = utils.run_single_prog(elf_path)
-        output2, status = utils.run_single_prog("node {}".format(js_path))
+        output1, status1 = utils.run_single_prog(elf_path)
+        output2, status2 = utils.run_single_prog("node {}".format(js_path))
 
         # debug 
         # input("continue?")
 
         # heavyweight checking
         # glob_correct, func_correct, glob_perf, func_perf = trace_consistency.trace_check(c_path, clang_opt_level='-O3', emcc_opt_level='-O3', need_compile=False)
-
-        if output1 != output2:
+        if status1 and not status2:
+            # x86 program did not return normally
+            print("Possible case: test{}-{}".format(process_idx, tmp_file_idx))
+            continue
+        elif not status1 and status2:
+            # wasm program did not return normally
+            print("Possible case: test{}-{}".format(process_idx, tmp_file_idx))
+            continue
+        elif output1 != output2:
             print("Possible case: test{}-{}".format(process_idx, tmp_file_idx))      
             continue
         else:
@@ -70,8 +80,8 @@ def trace_test(process_idx: int):
             wasm_path, js_path, wasm_dwarf_txt_path = profile.emscripten_dwarf(c_path, opt_level='-O0')
             elf_path, dwarf_path = profile.clang_dwarf(c_path, opt_level='-O3')
 
-            output1, status = utils.run_single_prog(elf_path)
-            output2, status = utils.run_single_prog("node {}".format(js_path))
+            output1, status1 = utils.run_single_prog(elf_path)
+            output2, status2 = utils.run_single_prog("node {}".format(js_path))
             if output1 == output2:
                 break  # exclude FPs caused by Undefined Behaviors
         # print('test{}-{}.c generated'.format(process_idx, tmp_file_idx))
@@ -79,8 +89,8 @@ def trace_test(process_idx: int):
         wasm_path, wasm_dwarf_txt_path = utils.wasm_opt(wasm_path, wasm_opt_level='-O3')
 
         # lightweight checking
-        output1, status = utils.run_single_prog(elf_path)
-        output2, status = utils.run_single_prog("node {}".format(js_path))
+        output1, status1 = utils.run_single_prog(elf_path)
+        output2, status2 = utils.run_single_prog("node {}".format(js_path))
 
         # heavyweight checking
         glob_correct, func_correct, glob_perf, func_perf = trace_consistency.trace_check(c_path, clang_opt_level='-O3', emcc_opt_level='-O3', need_compile=False)
@@ -106,16 +116,16 @@ def single_test(c_path: str):
     js_path = c_path[:-2] + '.js'
     wasm_path = c_path[:-2] + '.wasm'
 
-    wasm_path, js_path, wasm_dwarf_txt_path = profile.emscripten_dwarf(c_path, opt_level='-O0')
-    elf_path, dwarf_path = profile.clang_dwarf(c_path, opt_level='-O3')
+    wasm_path, js_path, wasm_dwarf_txt_path = profile.emscripten_dwarf(c_path, opt_level='-O1')
+    elf_path, dwarf_path = profile.clang_dwarf(c_path, opt_level='-O1')
 
-    output1, status = utils.run_single_prog(elf_path)
-    output2, status = utils.run_single_prog("node {}".format(js_path))
+    output1, status1 = utils.run_single_prog(elf_path)
+    output2, status2 = utils.run_single_prog("node {}".format(js_path))
 
-    wasm_path, wasm_dwarf_txt_path = utils.wasm_opt(wasm_path, wasm_opt_level='-O3')
+    wasm_path, wasm_dwarf_txt_path = utils.wasm_opt(wasm_path, wasm_opt_level='-all')
 
-    output1, status = utils.run_single_prog(elf_path)
-    output2, status = utils.run_single_prog("node {}".format(js_path))
+    output1, status1 = utils.run_single_prog(elf_path)
+    output2, status2 = utils.run_single_prog("node {}".format(js_path))
 
     glob_correct, func_correct, glob_perf, func_perf = trace_consistency.trace_check(c_path, clang_opt_level='-O3',
                                                                                      emcc_opt_level='-O3',
@@ -139,11 +149,11 @@ def worker2(sleep_time: int):
 
 
 if __name__ == '__main__':
-    # simple_test(0)
+    # simple_test(7)
     # trace_test(0)
     # single_test("./test15-4498.c")
     # single_test("./test6-1611.c")
-    single_test("./test11-585.c")
+    single_test("./test7-5233.c")
     exit(0)
 
     if len(sys.argv) == 2 and sys.argv[1] == '1':
